b6dd2b5e3b7f69bbbc68e2dc05afe37afb260495
jamiemalcolm <jmalcolm281@gmail.com>
2020-11-24 15:08:14 +0000

    own test written and passed

--- bus_lab_start_code/src/person.py
+++ bus_lab_start_code/src/person.py
@@ -1,4 +1,14 @@
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, destination):
         self.name = name
         self.age = age
+        self.destination = destination
+
+    def pick_right_bus(self, bus, bus_stop):
+        # make sure destination matches passenger
+        # update the queue
+        # update the buss passenger count
+        for passenger in bus_stop.queue:
+            if bus.destination == passenger.destination:
+                bus_stop.queue.remove(passenger)
+                bus.passengers.append(passenger)
--- bus_lab_start_code/tests/person_test.py
+++ bus_lab_start_code/tests/person_test.py
@@ -1,9 +1,12 @@
 import unittest
 from src.person import Person
+from src.bus import *
+from src.bus_stop import *
+
 
 class TestPerson(unittest.TestCase):
     def setUp(self):
-        self.person = Person("Guido van Rossum", 64)
+        self.person = Person("Guido van Rossum", 64, "blablabla")
 
     @unittest.skip("Delete this line to run the test")
     def test_person_has_name(self):
@@ -12,3 +15,13 @@ class TestPerson(unittest.TestCase):
     @unittest.skip("Delete this line to run the test")
     def test_person_has_age(self):
         self.assertEqual(64, self.person.age)
+
+    def test_is_the_right_bus__true(self):
+        person_1 = Person("Jock", 25, "Ocean Terminal")
+        person_2 = Person("Ann", 45, "Leith Links")
+        bus = Bus(22, "Ocean Terminal")
+        bus_stop = BusStop("Albert str")
+        bus_stop.add_to_queue(person_1)
+        bus_stop.add_to_queue(person_2)
+        self.person.pick_right_bus(bus, bus_stop)
+        self.assertEqual(1, bus.passenger_count())
